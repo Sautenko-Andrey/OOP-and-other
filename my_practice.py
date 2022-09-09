2dfe5067a6619438812f4e061c5dfa2d38a6ff3b
Andrey Sautenko <sautenkoandrey@gmail.com>
2022-09-09 16:20:22 +0300

    last changes for 09.09

--- my_practice.py
+++ my_practice.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class People:
     '''Main class for working with people,including friends, coworkers and other.'''
 
@@ -101,7 +104,7 @@ class Coworker(People):
     def adding_words(self, start='Sallary is: ', end='grn'):
         def decorator(func):
             def wrapper(x, *args, **kwargs):
-                res = func(x,)
+                res = func(x, )
                 res2 = start + str(res) + end
                 return res
 
@@ -127,6 +130,93 @@ class Coworker(People):
         print('We love our super best job!')
 
 
-chaus = Friend('alex chausov', 'male', 35, '+380950180611', 'best')
-kubasov = Coworker('kubasov artem', 'male', 35, 'engineer')
-print(kubasov.estimate_sallary(23))
+class Algoritms:
+    '''Here i practice my favourite algoritms,
+    including binary search, fast sort, BFS, bubble sort
+    and merge sort'''
+
+    def __init__(self, choose):
+        self.choice = choose
+
+    def binary_search(self, array, digit):
+        first_index = 0
+        last_index = len(array) - 1
+
+        while first_index <= last_index:
+            middle_index = (first_index + last_index) // 2
+            program_guess = array[middle_index]
+
+            if program_guess == digit:
+                return f'Here is index of <{digit}> --- {middle_index}!'
+            elif program_guess > digit:
+                last_index = middle_index - 1
+            else:
+                first_index = middle_index + 1
+
+        return f'There is not {digit}...'
+
+    def fast_sort(self, array):
+        if len(array) < 2:
+            return array
+        base_index = len(array) // 2
+        base_item = array[base_index]
+
+        low = [x for x in array[:base_index] if x < base_item] + [x for x in array[base_index + 1:] if x < base_item]
+        high = [x for x in array[:base_index] if x > base_item] + [x for x in array[base_index + 1:] if x > base_item]
+
+        return self.fast_sort(low) + [base_item] + self.fast_sort(high)
+
+    def __search_Shoker(self, name):
+        if name == 'Shoker':
+            return name
+
+    def BFS(self, dictionary, key):
+        self.__search_Shoker(dictionary[key])
+        turn = deque()
+        turn += dictionary[key]
+        verifed = []
+
+        while turn:
+            person = turn.popleft()
+            if not person in verifed:
+                if self.__search_Shoker(person):
+                    return f'We find fucking {person}!'
+                else:
+                    verifed.append(person)
+                    turn += dictionary[person]
+
+        return 'We didn\'t find Shoker....'
+
+    def bubble_sort(self,array):
+        count_items=len(array)
+        count_iterations=count_items-1
+
+        for iteration in range(0,count_iterations):
+            for dig in range(0,count_iterations-iteration):
+                if array[dig]>array[dig+1]:
+                    array[dig],array[dig+1]=array[dig+1],array[dig]
+
+        return array
+
+    def merge_sort(self,array1,array2):
+        mutual_array=[]
+
+        length_1=len(array1)
+        length_2=len(array2)
+
+        arrow1=0
+        arrow2=0
+
+        while length_1>arrow1 and length_2>arrow2:
+            if array1[arrow1]<array2[arrow2]:
+                mutual_array.append(array1[arrow1])
+                arrow1+=1
+            else:
+                mutual_array.append(array2[arrow2])
+                arrow2+=1
+
+        mutual_array+=array1[arrow1:]+array2[arrow2:]
+
+        return mutual_array
+
+
--- test_practice.py
+++ test_practice.py
@@ -1,4 +1,4 @@
-from my_practice import People, Coworker
+from my_practice import People, Coworker, Algoritms
 import pytest
 
 
@@ -14,7 +14,40 @@ def test_estimate_sallary():
     expect = 14375.0
     assert result == expect
 
+
 def test_estimate_sallary_bad():
     person = Coworker('denshik ruslan', 'male', 40, 'engineer')
     with pytest.raises(TypeError):
         result = person.estimate_sallary("23")
+
+
+def test_binary_search():
+    user = Algoritms('binary search')
+    result = user.binary_search([1, 2, 3, 4, 5], 4)
+    expected_result = 'Here is index of <4> --- 3!'
+    assert result == expected_result
+
+
+def test_binary_search_errors():
+    user = Algoritms('binary search')
+    with pytest.raises(TypeError):
+        result = user.binary_search([1, 2, 3, 4, 5], "4")
+
+
+def test_fast_sort():
+    user = Algoritms('fast sort')
+    result = user.fast_sort([10, 3, 1, 7, -2])
+    expected_result = [-2, 1, 3, 7, 10]
+    assert result == expected_result
+
+def test_bubble_sort():
+    user=Algoritms('bubble sort')
+    result=user.bubble_sort([3,1,7,2,100])
+    expected_result=[1,2,3,7,100]
+    assert result==expected_result
+
+def test_merge_sort():
+    user=Algoritms('merge sort')
+    result=user.merge_sort([1,3],[2,4])
+    expected_result=[1,2,3,4]
+    assert result==expected_result
